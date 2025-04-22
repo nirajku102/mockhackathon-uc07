a236e82b25fc11244df80172e5abbe4bf532f936
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-22 09:48:01 +0000

    test

--- infra/vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
--- infra/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS region for the VPC"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for the VPC"
-}
