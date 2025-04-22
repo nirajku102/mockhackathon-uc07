3713c8b00423e38c29d5fb09437c6f496f3f00a6
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-22 09:15:06 +0000

    build and push microservice image

---
+++ infra/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region for the VPC"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+}
