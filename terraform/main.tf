9fbd3b50135c86679bba6f3bece6186454cc0f68
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-22 09:43:03 +0000

    test

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-north-1"
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+
+
+module "vpc" {
+  source  = "./modules/vpc"
+  aws_region = "eu-north-1"
+  vpc_cidr   = "10.0.0.0/16"
+}
+
+module "eks" {
+  source          = "./modules/eks"
+  kubernetes_version = "1.21"
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  ami_id             = "AL2_x86_64"
+  instance_types     = ["t3.medium"]
+  aws_region         = "eu-north-1"  # Pass the region
+} 
